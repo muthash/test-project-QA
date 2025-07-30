41c2953c722e7495c26fe8322976161c5f98c925
muthama <muthamass@gmail.com>
2025-07-30 13:41:23 +0300

    Analyze attachments data for success

--- e2e/src/tests/db.spec.ts
+++ e2e/src/tests/db.spec.ts
@@ -1,6 +1,34 @@
-import prisma from "../../prisma";
-import { loginTest as test } from "../fixtures/loginPage.fixture";
+import { expect, test } from "@playwright/test";
 
-test("prisma", async ({}) => {
-  console.log(JSON.stringify(await prisma.role.findMany()));
+import prisma from "../../prisma";
+
+test.describe("Database Tests", () => {
+  test("prisma - users", async ({}, testInfo) => {
+    const users = await prisma.appUser.findMany({
+      include: {
+        phones: true,
+        roles: { include: { role: true } },
+      },
+    });
+
+    const data = JSON.stringify(users, null, 2);
+    await testInfo.attach("users.json", {
+      body: data,
+      contentType: "application/json",
+    });
+
+    expect(users.length).toBeGreaterThan(0);
+  });
+
+  test("prisma - roles", async ({}, testInfo) => {
+    const roles = await prisma.role.findMany();
+
+    const data = JSON.stringify(roles, null, 2);
+    await testInfo.attach("roles.json", {
+      body: data,
+      contentType: "application/json",
+    });
+
+    expect(roles.length).toBeGreaterThan(0);
+  });
 });
